bc12338ec97071f7c5366995dbd7f48338b8c932
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-02-12 17:24:40 +0300

    Add files via upload

---
+++ Exemple2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100) {
+    System.Console.WriteLine("Введено не трехзначное число, третьей цифры нет");
+}
+int[] arr = number.ToString().ToCharArray().Select(x => x - '0').ToArray();
+
+System.Console.WriteLine( arr[2] );
---
+++ Exemple3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите цифру дня недели, для проверки выходного: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 1)
+    {
+    Console.WriteLine("Нет");
+}
+else if (a == 2)
+    {
+    Console.WriteLine("Нет");
+}
+else if (a == 3)
+    {
+    Console.WriteLine("Нет");
+}
+else if (a == 4)
+    {
+    Console.WriteLine("Нет");
+}
+else if (a == 5)
+    {
+    Console.WriteLine("Нет");
+}
+else if (a == 6)
+    {
+    Console.WriteLine("Да");
+}
+else if (a == 7)
+    {
+    Console.WriteLine("Да!");
+}
+else
+{
+    Console.WriteLine("такого дня не существует");
+}
