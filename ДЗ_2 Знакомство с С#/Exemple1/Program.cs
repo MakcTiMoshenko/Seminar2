6ad2a2484f49c80d21b2c3a1824cb51fa19235dd
MaksTimoshenko <124436224+MakcTiMoshenko@users.noreply.github.com>
2023-02-12 15:29:10 +0300

    Add files via upload

---
+++ ДЗ_2 Знакомство с С#/Exemple1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+System.Console.WriteLine("Введите трехзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+if (number1 < 100) {
+    System.Console.WriteLine("Введено не трехзначное число");
+}
+if (number1 > 999) {
+    System.Console.WriteLine("Введено не трехзначное число");
+}
+int number2 = number1 / 10 % 10;
+System.Console.WriteLine("Второе число: " + number2);
